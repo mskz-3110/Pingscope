160ec94b38aab0242576c20ed53e7c5adb6a835a
mskz-3110 <mskz.saito@gmail.com>
2024-11-19 21:20:34 +0900

    Add Ping

--- Python/pingscope.py
+++ Python/pingscope.py
@@ -1,59 +1,130 @@
 import plotly.graph_objects as pgo
 import numpy as np
+import datetime as dt
+import time
+import math
+import json
+import os
+import ping3
 
 class Color:
   def __init__(self, value):
-    self._Value = value
-    self._R, self._G, self._B = [int(value[i: i + 2], 16) for i in range(1, 7, 2)]
+    self.__Value = value
+    self.__R, self.__G, self.__B = [int(value[i: i + 2], 16) for i in range(1, 7, 2)]
 
   @property
   def Value(self):
-    return self._Value
+    return self.__Value
 
   @property
   def R(self):
-    return self._R
+    return self.__R
 
   @property
   def G(self):
-    return self._G
+    return self.__G
 
   @property
   def B(self):
-    return self._B
+    return self.__B
 
 class Stats:
   def __init__(self, values):
-    self._Min = self._Max = self._Avg = 0
+    self.__Min = self.__Max = self.__Avg = 0
     if 0 < len(values):
-      self._Min = min(values)
-      self._Max = max(values)
-      self._Avg = round(sum(values) / len(values))
+      self.__Min = min(values)
+      self.__Max = max(values)
+      self.__Avg = round(sum(values) / len(values))
 
   @property
   def Min(self):
-    return self._Min
+    return self.__Min
 
   @property
   def Max(self):
-    return self._Max
+    return self.__Max
 
   @property
   def Avg(self):
-    return self._Avg
+    return self.__Avg
+
+class GroupStats:
+  def __init__(self, values, groupMaxCount = 0):
+    self.__Tick = 1
+    self.__Min = []
+    self.__Max = []
+    self.__Avg = []
+    count = len(values)
+    if groupMaxCount <= 0:
+      groupMaxCount = count
+    if count <= groupMaxCount:
+      self.__Avg = values
+    else:
+      self.__Tick = round(len(values) / groupMaxCount)
+      for groupValues in np.array_split(values, groupMaxCount):
+        stats = Stats(groupValues)
+        self.__Min.append(stats.Min)
+        self.__Max.append(stats.Max)
+        self.__Avg.append(stats.Avg)
+
+  @property
+  def Tick(self):
+    return self.__Tick
+
+  @property
+  def Min(self):
+    return self.__Min
+
+  @property
+  def Max(self):
+    return self.__Max
+
+  @property
+  def Avg(self):
+    return self.__Avg
+
+class Ping:
+  def __init__(self, dst):
+    startTime = time.perf_counter()
+    roundTripTime = ping3.ping(dst, timeout = 1, unit = "ms")
+    self.__ElapsedTime = time.perf_counter() - startTime
+    if not roundTripTime:
+      self.__RoundTripTime = 1000
+      self.__SleepTime = 0
+    else:
+      self.__RoundTripTime = math.ceil(roundTripTime)
+      self.__SleepTime = max(1 - self.__ElapsedTime, 0)
+
+  @property
+  def RoundTripTime(self):
+    return self.__RoundTripTime
+
+  @property
+  def ElapsedTime(self):
+    return self.__ElapsedTime
+
+  @property
+  def SleepTime(self):
+    return self.__SleepTime
+
+  @classmethod
+  def Timestamp(self, time = None):
+    if time is None:
+      time = dt.datetime.now()
+    return time.strftime("%y%m%d_%H%M%S")
 
 class PingRank:
   def __init__(self, value = 50):
-    self._Name = PingRank.ToName(value)
-    self._Color = PingRank.ToColor(self._Name)
+    self.__Name = PingRank.ToName(value)
+    self.__Color = PingRank.ToColor(self.__Name)
 
   @property
   def Name(self):
-    return self._Name
+    return self.__Name
 
   @property
   def Color(self):
-    return self._Color
+    return self.__Color
 
   @classmethod
   def ToName(cls, value):
@@ -84,57 +155,89 @@ class PingRank:
     return Color("#FF0000") # red
 
 class PingConfig:
-  def __init__(self, roundTripTimes):
-    self._RoundTripTimes = tuple(roundTripTimes)
-    self._Stats = Stats(roundTripTimes)
+  def __init__(self, roundTripTimes = []):
+    self.RoundTripTimes = roundTripTimes
 
   @property
   def RoundTripTimes(self):
-    return self._RoundTripTimes
+    return self.__RoundTripTimes
+
+  @RoundTripTimes.setter
+  def RoundTripTimes(self, roundTripTimes):
+    self.__RoundTripTimes = tuple(roundTripTimes)
+    self.__Stats = Stats(roundTripTimes)
 
   @property
   def Stats(self):
-    return self._Stats
+    return self.__Stats
 
   def ToFigure(self, xAxisMaxCount = 30, yAxisMaxCount = 30):
-    elapsedTimeTick = 1
+    groupStats = GroupStats([0])
     rank = PingRank()
-    minRoundTripTimes = []
-    maxRoundTripTimes = []
-    avgRoundTripTimes = [0]
-    if 0 < len(self._RoundTripTimes):
-      rank = PingRank(self._Stats.Avg)
-      avgRoundTripTimes = []
-      if len(self._RoundTripTimes) <= xAxisMaxCount:
-        for value in self._RoundTripTimes:
-          avgRoundTripTimes.append(value)
-      else:
-        elapsedTimeTick = round(len(self._RoundTripTimes) / xAxisMaxCount)
-        for roundTripTimes in np.array_split(self._RoundTripTimes, xAxisMaxCount):
-          stats = Stats(roundTripTimes)
-          minRoundTripTimes.append(stats.Min)
-          maxRoundTripTimes.append(stats.Max)
-          avgRoundTripTimes.append(stats.Avg)
+    if 0 < len(self.__RoundTripTimes):
+      rank = PingRank(self.__Stats.Avg)
+      groupStats = GroupStats(self.__RoundTripTimes, xAxisMaxCount)
     elapsedTimes = []
-    for i in range(1, len(avgRoundTripTimes)):
-      elapsedTimes.append(i * elapsedTimeTick)
-    elapsedTimes.append(len(self._RoundTripTimes))
-    title = """<b>[Pingscope]<br>{rankName}(Avg:{avg}ms Min:{min}ms Max:{max}ms)</b>""".format(rankName = rank.Name, avg = self._Stats.Avg, min = self._Stats.Min, max = self._Stats.Max)
+    for i in range(1, len(groupStats.Avg)):
+      elapsedTimes.append(i * groupStats.Tick)
+    elapsedTimes.append(len(self.__RoundTripTimes))
+    title = """<b>[Pingscope]<br>{rankName}(Avg:{avg}ms Min:{min}ms Max:{max}ms)</b>""".format(rankName = rank.Name, avg = self.__Stats.Avg, min = self.__Stats.Min, max = self.__Stats.Max)
     color = rank.Color
-    if elapsedTimeTick == 1:
-      figure = pgo.Figure(data = [pgo.Scatter(mode = "lines", x = elapsedTimes, y = avgRoundTripTimes, line = dict(color = color.Value, width = 5))])
+    if groupStats.Tick == 1:
+      figure = pgo.Figure(data = [pgo.Scatter(mode = "lines", x = elapsedTimes, y = groupStats.Avg, line = dict(color = color.Value, width = 5))])
     else:
       fillcolor = """rgba({r}, {g}, {b}, 0.15)""".format(r = color.R, g = color.G, b = color.B)
       figure = pgo.Figure(data = [
-        pgo.Scatter(showlegend = False, mode = "lines", x = elapsedTimes, y = minRoundTripTimes, line = dict(color = color.Value, width = 0)),
-        pgo.Scatter(showlegend = False, mode = "lines", x = elapsedTimes, y = avgRoundTripTimes, line = dict(color = color.Value, width = 5), fillcolor = fillcolor, fill = "tonexty"),
-        pgo.Scatter(showlegend = False, mode = "lines", x = elapsedTimes, y = maxRoundTripTimes, line = dict(color = color.Value, width = 0), fillcolor = fillcolor, fill = "tonexty"),
+        pgo.Scatter(showlegend = False, mode = "lines", x = elapsedTimes, y = groupStats.Min, line = dict(color = color.Value, width = 0)),
+        pgo.Scatter(showlegend = False, mode = "lines", x = elapsedTimes, y = groupStats.Avg, line = dict(color = color.Value, width = 5), fillcolor = fillcolor, fill = "tonexty"),
+        pgo.Scatter(showlegend = False, mode = "lines", x = elapsedTimes, y = groupStats.Max, line = dict(color = color.Value, width = 0), fillcolor = fillcolor, fill = "tonexty"),
       ])
-    figure.update_xaxes(tickformat = "d", dtick = elapsedTimeTick, linecolor = "black", linewidth = 3, gridcolor = "black", griddash = "dot", mirror = True)
+    figure.update_xaxes(tickformat = "d", dtick = groupStats.Tick, linecolor = "black", linewidth = 3, gridcolor = "black", griddash = "dot", mirror = True)
     figure.update_yaxes(tickformat = "d", dtick = 1, linecolor = "black", linewidth = 3, gridcolor = "black", griddash = "dot", mirror = True)
     figure.update_layout(title = dict(text = title, font = dict(color = color.Value, size = 26), x = 0.5),
       xaxis = dict(title = "Elapsed time(sec)"),
-      yaxis = dict(title = "Round trip time(ms)", range = [self._Stats.Min, min(self._Stats.Min + yAxisMaxCount, self._Stats.Max)]),
+      yaxis = dict(title = "Round trip time(ms)", range = [self.__Stats.Min, min(self.__Stats.Min + yAxisMaxCount, self.__Stats.Max)]),
       font = dict(color = "black", size = 14),
       plot_bgcolor = "white")
     return figure
+
+  def SaveImageFile(self, filePath, width = 1600, height = 900):
+    self.ToFigure().write_image(filePath, width = width, height = height)
+
+  def LoadPingFile(self, filePath):
+    roundTripTimes = []
+    with open(filePath) as file:
+      while True:
+        line = file.readline()
+        if not line:
+          break
+        if not line.startswith("#"):
+          roundTripTimes.append(int(line))
+    self.RoundTripTimes = roundTripTimes
+    return self
+
+  @classmethod
+  def OpenWriteFile(cls, filePath):
+    directoryPath = os.path.dirname(filePath)
+    if not os.path.isdir(directoryPath):
+      os.makedirs(directoryPath)
+    return open(filePath, mode = "w", newline = "\n")
+
+  @classmethod
+  def Run(cls, dst, count = 5, onLine = None):
+    startTime = dt.datetime.now()
+    if onLine is None:
+      onLine = lambda line: None
+    onLine("""#{line}\n""".format(line = json.dumps({"Dst": dst, "Count": count, "Timestamp": Ping.Timestamp(startTime)})))
+    roundTripTimes = []
+    ping = Ping(dst)
+    if ping.RoundTripTime < 1000:
+      for _ in range(count):
+        ping = Ping(dst)
+        roundTripTimes.append(ping.RoundTripTime)
+        onLine("""{line}\n""".format(line = ping.RoundTripTime))
+        time.sleep(ping.SleepTime)
+    elapsedTime = int((dt.datetime.now() - startTime).total_seconds())
+    stats = Stats(roundTripTimes)
+    onLine("""#{line}\n""".format(line = json.dumps({"Avg": stats.Avg, "Min": stats.Min, "Max": stats.Max, "ElapsedTime": elapsedTime})))
+    return PingConfig(roundTripTimes)
--- Python/test_pingscope.py
+++ Python/test_pingscope.py
@@ -1,17 +1,21 @@
 import pingscope as ps
 
 def test_OutputPng():
-  width = 1600
-  height = 900
-  ps.PingConfig([7, 8, 9, 10, 11]).ToFigure().write_image("../Image/SampleS.png", width = width, height = height)
-  ps.PingConfig([12, 13, 14, 15, 16]).ToFigure().write_image("../Image/SampleA.png", width = width, height = height)
-  ps.PingConfig([17, 18, 19, 20, 21]).ToFigure().write_image("../Image/SampleB.png", width = width, height = height)
-  ps.PingConfig([27, 28, 29, 30, 31]).ToFigure().write_image("../Image/SampleC.png", width = width, height = height)
-  ps.PingConfig([47, 48, 49, 50, 51]).ToFigure().write_image("../Image/SampleD.png", width = width, height = height)
-  ps.PingConfig([48, 49, 1000, 51, 52]).ToFigure().write_image("../Image/SampleE.png", width = width, height = height)
-  ps.PingConfig([]).ToFigure().write_image("../Image/Sample0.png", width = width, height = height)
+  ps.PingConfig([7, 8, 9, 10, 11]).SaveImageFile("../Image/SampleS.png")
+  ps.PingConfig([12, 13, 14, 15, 16]).SaveImageFile("../Image/SampleA.png")
+  ps.PingConfig([17, 18, 19, 20, 21]).SaveImageFile("../Image/SampleB.png")
+  ps.PingConfig([27, 28, 29, 30, 31]).SaveImageFile("../Image/SampleC.png")
+  ps.PingConfig([47, 48, 49, 50, 51]).SaveImageFile("../Image/SampleD.png")
+  ps.PingConfig([48, 49, 1000, 51, 52]).SaveImageFile("../Image/SampleE.png")
+  ps.PingConfig([]).SaveImageFile("../Image/Sample0.png")
 
   roundTripTimes = []
   for i in range(3600):
     roundTripTimes.append(i % 30 + 10)
-  ps.PingConfig(roundTripTimes).ToFigure().write_image("../Image/Sample3600.png", width = width, height = height)
+  ps.PingConfig(roundTripTimes).SaveImageFile("../Image/Sample3600.png")
+
+#def test_Ping():
+#  pingFilePath = """../Image/{timestamp}.ping""".format(timestamp = ps.Ping.Timestamp())
+#  with ps.PingConfig.OpenWriteFile(pingFilePath) as file:
+#    ps.PingConfig.Run("localhost", onLine = lambda line: file.write(line)).SaveImageFile("../Image/PingSave.png")
+#  ps.PingConfig().LoadPingFile(pingFilePath).SaveImageFile("../Image/PingLoad.png")
